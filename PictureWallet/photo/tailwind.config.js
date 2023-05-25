6556447be976807ff0500bfd1a758a0f415fabd3
Md Anisur Rahman <anisur.pythondevloper@gmail.com>
2023-05-25 18:04:41 +0600

    PictureWallet using Taildwindcss

---
+++ PictureWallet/photo/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    colors:{
+      "top_nav":"#232228",
+      "sub_nav":"#35353d",
+      "body":"#3c3c44",
+      "font_color": "#FFFFFF",
+      "scroll_color": "#808080",
+    }
+  },
+  plugins: [],
+}
+
